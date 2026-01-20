93b16080eca487e3d815fa440709cfad1d767b35
santiagochrisjavi-dev <santiagochrisjavi@icloud.com>
2026-01-20 05:59:17 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Dispute Letter Pro</h1>
+      <p>Payment success will redirect here.</p>
+    </main>
+  );
+}
