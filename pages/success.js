f7b7a0ef8be6886d6173f6bc432d6a1250e03163
santiagochrisjavi-dev <santiagochrisjavi@icloud.com>
2026-01-20 06:00:17 -0500

    Create success.js

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Payment Successful</h1>
+      <p>Your letter will be generated next.</p>
+    </main>
+  );
+}
